b7f0ac3cae9bc998b7afa9a47a95bcdc861d9bd9
showsjohn <showsjohn@gmail.com>
2025-02-13 23:03:11 -0600

    final commit

--- src/main/java/com/moviereview/api/config/CorsConfig.java
+++ src/main/java/com/moviereview/api/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:63342", "http://localhost:8080")
+                        .allowedOrigins("http://localhost:63342", "http://localhost:8080", "http://localhost:8090")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
